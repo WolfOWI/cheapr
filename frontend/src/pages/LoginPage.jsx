dd599cfa9998e84029cf9036299bf35939dff6f8
WolfOWI <97434628+WolfOWI@users.noreply.github.com>
2024-09-24 14:52:46 +0200

    Login Basic Elements

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -10,10 +10,13 @@
 // -
 
 // Third-Party Components
-// -
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 
 // Internal Components
 import NavigationBar from "../components/navigation/NavigationBar";
+import Btn from "../components/button/Btn";
+import { Container } from "react-bootstrap";
 
 // Imagery
 // -
@@ -23,7 +26,35 @@ function LoginPage() {
   return (
     <>
       <NavigationBar />
-      <div className="">LoginPage</div>
+      <div className="w-full h-dvh bg-neutral-600 flex">
+        <Container>
+          <div className="bg-neutral-50 w-[500px] rounded-2xl p-10 h-fit mt-24">
+            <h2>Welcome</h2>
+            <h4>Please enter your login details below.</h4>
+            <Form>
+              <Form.Group className="mt-8 mb-4" controlId="formBasicEmail">
+                <Form.Label className="font-semibold">Email address</Form.Label>
+                <Form.Control
+                  type="email"
+                  placeholder="hello@gmail.com"
+                  className="h-12 rounded-xl"
+                />
+              </Form.Group>
+
+              <Form.Group className="mb-4" controlId="formBasicPassword">
+                <Form.Label className="font-semibold">Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" className="h-12 rounded-xl" />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                <Form.Check type="checkbox" label="I am an admin" />
+              </Form.Group>
+              <Btn variant="primary" className="w-full" type="submit">
+                Login
+              </Btn>
+            </Form>
+          </div>
+        </Container>
+      </div>
     </>
   );
 }
